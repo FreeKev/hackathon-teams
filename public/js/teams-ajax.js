1e7248539cbedb268c8f8166d4fc3636da6266d5
Joe Johnson <joe@Joes-MacBook-Pro.local>
2017-12-21 22:25:05 -0800

    Created delete button and DELETE route.

--- public/js/teams-ajax.js
+++ public/js/teams-ajax.js
@@ -1,3 +1,21 @@
+// var express = require('express');
+// var router = express.Router();
+
 $(document).ready(function() {
 
+
+
 });
+
+console.log('Hi from routing');
+// $('.delete-link').click(function(e){
+$('.delete-link').click(function(e){    
+    e.preventDefault();
+    console.log('clicked');
+    $.ajax({
+      url: $(this).attr('href'),
+      method: 'DELETE'
+    }).done(function(data){
+      window.location.href = '/teams';
+    });
+  });
